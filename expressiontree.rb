73f4fd101cdcfd1707bb2e5c359fa63fe9566a6b
Gregory H. Halverson <gregory.halverson@gmail.com>
2015-03-06 17:43:33 -0800

    added exponents

--- expressiontree.rb
+++ expressiontree.rb
@@ -2,12 +2,17 @@ module ExpressionTree
   class Expression
     def initialize
       @operation = nil
+      @operator = nil
       @numeric_operator = nil
       @operands = nil
     end
 
     def operation
       @operation
+    end
+
+    def operator
+      @operator
     end
 
     def numeric_operator
@@ -19,7 +24,7 @@ module ExpressionTree
     end
 
     def evaluate_operands
-      @operands.collect { |operand| operand.is_a?(Expression) ? operand.evaluate : operand }
+      self.operands.collect { |operand| operand.is_a?(Expression) ? operand.evaluate : operand }
     end
 
     def evaluate
@@ -28,63 +33,74 @@ module ExpressionTree
     end
 
     def numeric_operands?
-      @operands.all? { |operand| operand.is_a? Numeric }
+      self.operands.all? { |operand| operand.is_a? Numeric }
+    end
+
+    def operand_representations
+      self.operands.collect do |operand|
+        enclose = false
+        enclose = true if operand.is_a? Expression
+
+        if operands.is_a? Numeric
+          enclose = true if operand < 0
+        end
+
+        representation = operand.to_s
+
+        enclose ? '(' + representation + ')' : representation
+      end
+    end
+
+    def infix
+      self.operand_representations.join(' ' + self.operator + ' ')
+    end
+
+    def to_s
+      self.infix
     end
   end
-
-
 
   class Sum < Expression
     def initialize(operands)
       @operation = :sum
+      @operator = '+'
       @numeric_operator = :+
       @operands = operands
-    end
-
-    def to_s
-      '(' + @operands.join(' + ') + ')'
     end
   end
 
   class Subtraction < Expression
     def initialize(operands)
       @operation = :subtraction
+      @operator = '-'
       @numeric_operator = :-
       @operands = operands
-    end
-
-    def to_s
-      '(' + @operands.join(' - ') + ')'
     end
   end
 
   class Multiplication < Expression
     def initialize(operands)
       @operation = :multiplication
+      @operator = '-'
       @numeric_operator = :*
       @operands = operands
-    end
-
-    def to_s
-      '(' + @operands.join(' * ') + ')'
     end
   end
 
   class Division < Expression
     def initialize(operands)
       @operation = :multiplication
+      @operator = '-'
       @numeric_operator = :/
       @operands = operands
-    end
-
-    def to_s
-      '(' + @operands.join(' / ') + ')'
     end
   end
 
   class Exponentiation < Expression
     def initialize(base, exponent)
       @operation = :exponentiation
+      @operator = '^'
+      @numeric_operator = :**
       @operands = [base, exponent]
     end
 
@@ -97,12 +113,8 @@ module ExpressionTree
     end
 
     def evaluate
-      evaluated = Exponent.new(self.evaluate_operands)
-      evaluated.numeric_operands? ? base ** exponent : evaluated
-    end
-
-    def to_s
-      "#{base}^#{exponent}"
+      evaluated = Exponentiation.new(*self.evaluate_operands)
+      evaluated.numeric_operands? ? evaluated.base ** evaluated.exponent : evaluated
     end
   end
 
--- test.rb
+++
@@ -1,9 +0,0 @@
-require_relative 'expressiontree.rb'
-
-include ExpressionTree
-
-s = sum(mul(2, 3), 2)
-
-puts s
-puts s.evaluate
-puts s.class
